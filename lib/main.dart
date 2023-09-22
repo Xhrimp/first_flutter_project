0d9389d45e4c7b5f607bdea1ac9bef1e3425ffc2
Xhrrimp <hongphuc152000@gmail.com>
2023-09-22 17:03:46 +0700

    22/9/2023

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,50 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+  final int test = 0;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //declare nullable variable
+  int? number;
+
+  @override
+  void initState() {
+    number = widget.test;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (number != null) {
+                        number = number! + 1; //ensure that variable is not null
+                      }
+                    });
+                  },
+                  child: Text("$number")),
+              ElevatedButton(onPressed: () {}, child: const Text("data")),
+            ],
+          ),
         ),
       ),
     );
